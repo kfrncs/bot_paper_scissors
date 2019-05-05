db5603b2aca9e0d47899bbe98a01862119652782
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-05 19:29:21 -0400

    create df on 10th turn. move functionality from app.py to kenbot.py

--- app.py
+++ app.py
@@ -6,36 +6,24 @@ import random
 
 import joblib
 from utils import emoji_to_num, evaluate
-from bots.kenbot import KenBot 
+from bots.kenbot import KenBot
 
 app = Flask(__name__)
-bot = KenBot('bots/rock_paper_forests.joblib')
-# read blank.csv as template for game data
-results = pd.read_csv('data/blank.csv')
+
 game_count = 0
+
+bot = KenBot('bots/rock_paper_forests.joblib')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     game = {}
     if request.method == 'POST':
-        # choose random row to play from for the first five turns
-        # rand_row = np.random.randint(1,50000)
-        # df.iloc[rand_row:rand_row+5]
-        player_throw = emoji_to_num(request.form['player_throw'])
 
-        # under game 5 we throw randomly, otherwise we call bot.throw()
-        if bot.game_count < 5:
-            bot_throw = random.choice([1, 2, 3]) 
-        elif bot.game_count == 5:
-            # TODO: this is where we will create the past five turns df
-            print('error out on game 5')
-            bot_throw = bot.throw(results)
-            pass
-        else:
-            bot_throw = bot.throw(results) # 
-        
-        bot.game_count += 1
-        
+        player_throw = emoji_to_num(request.form['player_throw'])
+        bot.history['player'].append(player_throw)
+
+        bot_throw = bot.throw()
+
         result = evaluate(player_throw, bot_throw)
 
         # TODO: append results to dataframe
--- bots/kenbot.py
+++ bots/kenbot.py
@@ -1,6 +1,7 @@
 # third, random bot
 import random
 import joblib
+import pandas as pd
 
 class KenBot:
     def __init__(self, model_joblib):
@@ -10,13 +11,65 @@ class KenBot:
 
     def capture(self, y):
         self.history['player'].append(y)
+        pass
 
     def predict(self):
         y = random.choice([1, 2, 3])
         self.history['bot'].append(y)
         return y
 
-    def throw(self, y):
-        x = self.model.predict(y)
-        self.capture(y)
+    def throw(self):
+        # x = self.model.predict(self.df)
+
+        if self.game_count < 10:
+            x = random.choice([1, 2, 3])
+            self.history['bot'].append(x)
+            pass
+        elif self.game_count == 10:
+            # TODO: this is where we will create the past five turns df
+
+            x = random.choice([1, 2, 3])
+            self.history['bot'].append(x)
+            self.create_df()
+            print('dataframe created')
+            # x = bot.throw(results)
+            pass
+        else:
+            #TODO:
+            # append this turn to the DF created
+            # print(bot.df)
+            # x = bot.predict(bot.df) #
+            pass
+        self.game_count += 1
         return x
+
+    def create_df(self):
+        print(f"length bot: ", len(self.history['bot']))
+        print(f"length player: ", len(self.history['bot']))
+
+        self.df = pd.DataFrame({
+            'player_one_throw': self.history['player'],
+            'p2_last': self.history['bot'] # bot's last turn.. almost
+        })
+
+
+        # make p2_last last turn instead of current
+        self.df['p2_last'] = self.df['p2_last'].shift(1)
+
+        # add the p1 history columns
+        for i in range(1,6):
+            self.df[f'p1_-{i}'] = self.df['player_one_throw'].shift(i)
+
+        self.df = self.df[['player_one_throw',
+                           'p1_-1',
+                           'p1_-2',
+                           'p1_-3',
+                           'p1_-4',
+                           'p1_-5',
+                           'p2_last']]
+
+        # and drop the first row (it doesn't have a p2_last)
+        self.df = self.df.drop(range(0,4))
+
+
+        return self
